92a42db25d276af528433e4fc941500c9922401b
Tetz <xenosign@naver.com>
2025-04-14 11:46:55 +0900

    250414 수업 준비

---
+++ src/test/variable/Variable01.java
@@ -0,0 +1,18 @@
+package test.variable;
+
+public class Variable01 {
+    public static void main(String[] args) {
+        int d = 10;
+        char e = 'e';
+        System.out.println(d + e);
+        System.out.println("한글 테스트");
+
+        int i = 1;
+        byte b = 127;
+
+        while (i < 1000) {
+            System.out.println(b--);
+            i++;
+        }
+    }
+}
